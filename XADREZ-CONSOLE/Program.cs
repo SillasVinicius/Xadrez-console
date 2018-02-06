a0c14a9016a65eeb85c67c6e1a1c6a7a6ef13680
Sillas Vinícius <sillasvinicius0403@gmail.com>
2018-02-05 22:54:39 -0200

    Criando a classe Posicao

--- XADREZ-CONSOLE/Program.cs
+++ XADREZ-CONSOLE/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using tabuleiro;
 
 namespace XADREZ_CONSOLE
 {
@@ -10,6 +11,12 @@ namespace XADREZ_CONSOLE
     {
         static void Main(string[] args)
         {
+            Posicao p;
+
+            p = new Posicao(3, 4);
+
+            Console.WriteLine("Posicao: " + p);
+            Console.ReadLine();
         }
     }
 }
---
+++ XADREZ-CONSOLE/tabuleiro/Posicao.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabuleiro
+{
+    class Posicao
+    {
+        public int linha { get; set; }
+        public int coluna { get; set; }
+
+        public Posicao(int linhaC, int colunaC)
+        {
+            this.linha = linhaC;
+            this.coluna = colunaC;
+        }
+
+        public override string ToString()
+        {
+            return linha
+                + ", "
+                + coluna;
+        }
+    }
+}
