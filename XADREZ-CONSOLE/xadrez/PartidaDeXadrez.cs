45fd3ebb784da24dd7fd1225585cffccca594dc4
Sillas Vinícius <sillasvinicius0403@gmail.com>
2018-02-08 22:26:01 -0200

    Implementando as restricoes de movimento

--- XADREZ-CONSOLE/xadrez/PartidaDeXadrez.cs
+++ XADREZ-CONSOLE/xadrez/PartidaDeXadrez.cs
@@ -10,8 +10,8 @@ namespace xadrez
     class PartidaDeXadrez
     {
         public Tabuleiro tab { get; private set; }
-        private int turno;
-        private Cor jogadorAtual;
+        public int turno { get; private set; }
+        public Cor jogadorAtual { get; private set; }
         public bool terminada { get; private set; }
 
         public PartidaDeXadrez() 
@@ -46,6 +46,52 @@ namespace xadrez
             p.incrementarQteMovimentos();
             Peca pecaCapturada = tab.retirarPeca(destino);
             tab.colocarPeca(p, destino);
+        }
+
+        public void realizaJogada(Posicao origem, Posicao destino) 
+        {
+            executaMovimento(origem, destino);
+            turno++;
+            mudaJogador();
+        }
+
+        public void validarPosicaoOrigem(Posicao pos) 
+        {
+            if (tab.peca(pos) == null)
+            {
+                throw new TabuleiroException("Não existe peca na posição de origem escolhida!");
+            }
+
+            if (jogadorAtual != tab.peca(pos).cor)
+            {
+                throw new TabuleiroException("A peca de origem escolhida não é sua!");
+            }
+
+            if (!tab.peca(pos).existeMovimentosPossiveis())
+            {
+                throw new TabuleiroException("Não há movimentos possiveis para a peça de origem escolhida!");
+            }
+
+        }
+
+        public void validarPosicaoDestino(Posicao origem, Posicao destino) 
+        {
+            if (!tab.peca(origem).podeMover(destino))
+            {
+                throw new TabuleiroException("Posição de destino inválida!");
+            }
+        }
+
+        public void mudaJogador() 
+        {
+            if (jogadorAtual == Cor.Branca)
+            {
+                jogadorAtual = Cor.Preta;
+            }
+            else
+            {
+                jogadorAtual = Cor.Branca;    
+            }
         }
     }
 }
